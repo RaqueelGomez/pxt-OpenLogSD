6e49d977e9a01b84fc09fddfdd213135abc8a328
RaqueelGomez <38318688+RaqueelGomez@users.noreply.github.com>
2019-07-14 23:35:13 +0200

    Creadas primera versión de la librería OpenLog SD

---
+++ main.ts
@@ -0,0 +1,41 @@
+/**
+ * Blocks for control openLog SD board
+ */
+//% weight=100 color=#00A654 icon="\uf085" block="Serial OpenLog"
+namespace OpenLog_SD {
+
+    //Some useful parameters. 
+    let baud = 115200      //default baudrate 
+    let initalised = false //a flag to allow us to initialise without explicitly calling the secret incantation
+
+    //nice big list of servos for the block to use. These represent register offsets in the PCA9865
+    export enum baudrate {
+        baud1 = 9600,
+        baud2 = 115200,
+    }
+
+    /**
+     * set the communication speed
+     *
+     * @param Valiue To choose the value
+     */
+    //% blockId=set_Baudrate
+    //% block="set%Baudrate"
+    //% Value.min=9600 Value.max=115200
+    export function SetBaudrate(Value: number) {
+        if (Value == 1) {
+            baud = baudrate.baud1
+        }
+        else if (Value == 2) {
+            baud = baudrate.baud1
+        }
+    }
+
+    function enviarTitulo(): void {
+        serial.writeLine("Tiempo, Muestras")
+        initalised = true
+    }
+
+    //export function servoWrite(Servo: Servos, degrees: number): void {
+
+} 
